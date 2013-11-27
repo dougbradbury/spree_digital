380708add5788e47b12e190022e5bffad6d2a78b
dougbradbury <doug@8thlight.com>
2013-11-26 22:12:04 -0600

    order#show is now gone in favor of order#edit, fix reset link

--- app/overrides/add_reset_digitals_to_admin_orders.rb
+++ app/overrides/add_reset_digitals_to_admin_orders.rb
@@ -1,7 +1,7 @@
 Deface::Override.new(:virtual_path => "spree/admin/shared/_order_tabs",
                      :name => "add_reset_digitals_to_admin_orders",
-                     :insert_after => ".sidebar",
+                     :insert_bottom => "[data-hook=admin_order_tabs]",
                      :text => %q{
-<%= content_tag(:p, button_link_to(t(:reset_downloads), reset_digitals_admin_order_url(@order)), class: 'clear') if @order.digital? or true %>
+<%= content_tag(:li, link_to(t(:reset_downloads), reset_digitals_admin_order_url(@order))) if @order.digital? %>
                       },
                      :disabled => false)
